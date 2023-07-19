bb3db578877e83e475ae2d2ccd36da4b9742fa4a
jnmgroups <139941009+jnmgroups@users.noreply.github.com>
2023-07-19 10:02:06 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,9 +22,9 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
+		js.put("Name", "JNM Groups");
+		js.put("Calling Name", "JNMGroups");
+		js.put("DOB", "08-Aug-2021");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
 
