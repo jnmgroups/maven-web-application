f02b3c82801014969301a99a38cea735decd781d
jnmgroups <kdineshreddy.devops@gmail.com>
2023-07-21 12:22:20 +0530

    uweifhdsdbaskj

--- EmployeeService.java
+++ EmployeeService.java
@@ -2,3 +2,6 @@ This file ias JAva file
 Update from GIT remote repo
 Testing git pull command:wq
 Updated from VS Code to test push
+Updated from Git to test fetch & merge
+Updated to test revert & rebase
+To test soft, hard, mixed Reset
